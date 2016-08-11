414062d8ca57df39d2f27928a253e15054d94a9c
User <User@User-PC>
2016-08-11 15:09:21 +0900

    initial commit

---
+++ src/kr/co/kosta/contact/ContactManagerMain.java
@@ -0,0 +1,14 @@
+package kr.co.kosta.contact;
+
+import kr.co.kosta.contact.view.ContactView;
+
+public class ContactManagerMain {
+
+	public static void main(String[] args) throws Exception {
+		
+		ContactView view = new ContactView();
+		view.run();
+		
+	}
+
+}
